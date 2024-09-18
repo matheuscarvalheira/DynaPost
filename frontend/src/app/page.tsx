7c144286e38a7d3821e0e43d2ecf3f93edc2e070
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-09-17 22:59:45 -0300

    Adjust components: Input, Textarea, and Modal

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,22 @@
-import { HomeTemplate } from "@/templates/home";
+"use client";
+
+import React, { useState } from 'react';
+import { Input } from '@/components/input';
+import { Textarea } from "@/components/textarea";
+import { Modal } from '@/components/modal';
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <div>
-      <HomeTemplate/>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <Input placeholder="Buscar..." showIcon={true} />
+        <Input placeholder="Digite algo..." showIcon={false} />
+        <Textarea placeholder="Digite" />
+      </Modal>
     </div>
   );
 }
