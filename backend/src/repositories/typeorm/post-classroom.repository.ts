66cd202665ef3f4282d097dd48d831c8e2f1d3cf
Mário André Martins <mario.andre.m@gmail.com>
2024-09-18 21:19:50 -0300

    get posts by classroom id

--- backend/src/repositories/typeorm/post-classroom.repository.ts
+++ backend/src/repositories/typeorm/post-classroom.repository.ts
@@ -3,11 +3,14 @@ import { appDataSource } from '@/lib/typeorm/typeorm'
 import { EntityManager, Repository } from 'typeorm'
 import { IPostClassroomRepository } from '../post-classroom.repository.interface'
 import { IPostClassroom } from '@/entities/models/post-classroom.interface'
+import { IPost } from '@/entities/models/post.interface'
+import { Post } from '@/entities/post.entity'
 
 export class PostClassroomRepository implements IPostClassroomRepository {
   private repository: Repository<PostClassroom>
+  private postRepository: Repository<Post>
 
-  constructor(transactionManager: EntityManager) {
+  constructor(transactionManager?: EntityManager) {
     if (transactionManager) {
       this.repository = transactionManager.getRepository(PostClassroom)
     } else {
@@ -17,5 +20,17 @@ export class PostClassroomRepository implements IPostClassroomRepository {
 
   async create(postClassroom: IPostClassroom): Promise<IPostClassroom> {
     return this.repository.save(postClassroom)
+  }
+
+  async findPostsByClassroom(id: string): Promise<(IPost | null)[]> {
+    const posts = await this.repository.find({ where: { classroom_id: id } })
+
+    const postIds = posts.map((post) => post.post_id)
+
+    const postData = await Promise.all(
+      postIds.map(async (id) => this.postRepository.findOne({ where: { id } })),
+    )
+
+    return postData
   }
 }
