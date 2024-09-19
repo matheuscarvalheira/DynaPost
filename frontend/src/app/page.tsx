a02934cbc04fc32ec9b39a1b6049d4e6a363febb
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-09-18 22:04:14 -0300

    clean up

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,11 @@
 "use client";
 
-import React, { useState } from 'react';
-import { Input } from '@/components/input';
-import { Textarea } from "@/components/textarea";
-import { Modal } from '@/components/modal';
+import { HomeTemplate } from '@/templates/home';
 
 export default function Home() {
-  const [isModalOpen, setIsModalOpen] = useState(true);
-
-  const closeModal = () => setIsModalOpen(false);
-
   return (
     <div>
-      <Modal isopen={isModalOpen} onClose={closeModal}>
-        <Input placeholder="Buscar..." showIcon={true} />
-        <Input placeholder="Digite algo..." showIcon={false} />
-        <Textarea placeholder="Digite" />
-      </Modal>
+      <HomeTemplate/>
     </div>
   );
 }
