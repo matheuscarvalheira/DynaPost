b098a6ab9c82c65d0bae53b764b774d41ef04726
Mário André Martins <mario.andre.m@gmail.com>
2024-09-18 22:09:40 -0300

    front end radio buttons (#18)
    * front end radio buttons
    
    * adjustments
    
    * double quotes to single

---
+++ frontend/src/components/radio-buttons/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useState } from 'react';
+import * as S from './styles';
+import { RadioButtonProps } from './props';
+
+export const RadioButtons = ({options}: RadioButtonProps) => {
+    const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+    const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSelectedOption(e.target.value);
+    };
+
+    return (
+        <S.RadioButtonsWrapper>
+            {options?.length > 0 && options.map((option) => 
+                <S.StyledLabel key={option.value} $isChecked={option.value === selectedOption}>
+                    <input 
+                        type="radio"
+                        value={option.value}
+                        checked={option.value === selectedOption}
+                        onChange={handleOptionChange}
+                    />
+                    {option.label}
+                </S.StyledLabel>
+            )}
+        </S.RadioButtonsWrapper>
+    )
+}
