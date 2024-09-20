e4b41eaebc59a304bf4012e6fbdd8640498a62db
First Last <lais_vieira@icloud.com>
2024-09-19 22:53:32 -0300

    Ajustes componente modal

--- frontend/src/components/modal/index.tsx
+++ frontend/src/components/modal/index.tsx
@@ -1,40 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
-
-interface ModalProps {
-  isopen: boolean;
-  onClose: () => void;
-  children: React.ReactNode;
-}
-
-const ModalBackground = styled.div<{ isopen: boolean }>`
-  display: ${({ isopen }) => (isopen ? 'flex' : 'none')};
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-`;
-
-const ModalContent = styled.div`
-  background-color: #202024;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  color: white;
-  max-width: 500px;
-  width: 100%;
-`;
+import { ModalProps } from './props';
+import { CloseModal, ModalBackground, ModalContent } from './styles';
+import { IoIosClose } from 'react-icons/io';
 
 export const Modal: React.FC<ModalProps> = ({ isopen, onClose, children }) => {
+  const showIcon = true; // Declare and assign a value to 'showIcon'
+  
   return (
-    <ModalBackground isopen={isopen} onClick={onClose}>
+    <ModalBackground isopen={isopen}>
       <ModalContent onClick={(e) => e.stopPropagation()}>
-        {children}
+      {children}
+        <CloseModal onClick={(e) => e.stopPropagation()}>
+          <IoIosClose />
+        </CloseModal>
       </ModalContent>
     </ModalBackground>
   );
