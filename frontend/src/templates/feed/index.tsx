6d41f44b78cf74a1b9049198c1d9fc12014754c3
ana.costa <ana.costa@fiap.com.br>
2024-09-26 21:59:34 -0300

    feat(Feed): estrutura da pagina

--- frontend/src/templates/feed/index.tsx
+++ frontend/src/templates/feed/index.tsx
@@ -1,11 +1,23 @@
 import { FC } from "react";
-import * as S from "./styles";
+import { Post } from "@/components/post";
+import * as S from './styles';
+import { Header } from "@/components/header";
+import { Dropdown } from "@/components/dropdown";
+import { Button } from "@/components/button";
 
 export const FeedTemplate: FC = () => {
-
-    return (
-        <S.Main>
-            Oi
-        </S.Main>
-    )
+  return (
+    <S.Home>
+      <Header/>
+      <Dropdown items={['Turma 1', 'Turma 2', 'Turma 3']}/>
+      <Button text={'cadastrar'} onClick={() => console.log('cadastrar')}/>
+      <Button buttonType="open-modal" onClick={() => console.log('abrir')}/>
+      <Button text={'apagar'} buttonType="delete-post" onClick={() => console.log('apagar')}/>
+      <S.Feed>
+        <Post />
+        <Post />
+        <Post />
+      </S.Feed>
+    </S.Home>
+  )
 }
--- frontend/src/templates/feed/styles.ts
+++ frontend/src/templates/feed/styles.ts
@@ -1,29 +1,57 @@
+import { Container, Post } from "@/components/post/styles";
 import styled from "styled-components";
 
-export const Main = styled.main`
-    height: 100%;
+export const Home = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 50px;
+`;
+
+export const Feed = styled.section`
+  width: 750px;
+  margin-top: 30px;
+  background-color: ${({ theme }) => theme.blackRaisin};
+  padding: 43px 35px;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 90px;
+
+  @media (max-width: 800px) {
     width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-`
+  }
 
-export const Form = styled.form`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    input {
-        max-width: 380px;
-        width: 80vw;
-        margin-bottom: 30px;
-    }
-    button {
-        margin: unset;
-    }
-`
+  @media (max-width: 600px) {
+    background-color: transparent;
+    gap: 50px;
+    padding: 0;
+  }
 
-export const Title = styled.h2`
-    margin-bottom: 64px;
-`
+  ${Post} {
+    cursor: pointer;
+
+    &:not(:last-child) {
+      &:after {
+        content: '';
+        height: 1px;
+        width: 100%;
+        display: block;
+        position: relative;
+        top: 45px;
+        background-color: ${({ theme }) => theme.arsenic};
+        cursor: auto;
+
+        @media (max-width: 600px) {
+          top: 25px;
+        }
+      }
+    }
+
+    ${Container} {
+      @media (max-width: 600px) {
+        padding: 0 40px;
+      }
+    }
+  }
+`;
