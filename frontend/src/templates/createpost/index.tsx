0cb00f25d3ae4ec1f09d7b4bbe7e712aafb4e44e
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-10-01 00:32:41 -0300

    unable code

--- frontend/src/templates/createpost/index.tsx
+++ frontend/src/templates/createpost/index.tsx
@@ -1,31 +1,31 @@
 
-import { FC, useState } from "react";
-import * as S from "./styles"
+// import { FC, useState } from "react";
+// import * as S from "./styles"
 
-export const CreatePostTemplate: FC = () => {
-    const [inputValue, setInputValue] = useState('');
-    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setInputValue(event.target.value);
-      };
+// export const CreatePostTemplate: FC = () => {
+//     const [inputValue, setInputValue] = useState('');
+//     const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+//         setInputValue(event.target.value);
+//       };
 
-    return (
-        <div>
-            <S.Modal isopen={true} onClose={() => {}}>
-                <div>
-                    <S.Dropdown 
-                    items={['Turma 1', 'Turma 2', 'Turma 3']}/>
-                    <S.Input 
-                        placeholder="Título" 
-                        required type="text" 
-                        name="title" 
-                        autocomplete="off"/>
-                    <S.Textarea 
-                        placeholder="Conteúdo" 
-                        value={inputValue}
-                        onChange={handleChange} />
-                    <S.Button type="submit" text="Publicar"/>
-                </div>
-            </S.Modal>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <S.Modal isopen={true} onClose={() => {}}>
+//                 <div>
+//                     <S.Dropdown 
+//                     items={['Turma 1', 'Turma 2', 'Turma 3']}/>
+//                     <S.Input 
+//                         placeholder="Título" 
+//                         required type="text" 
+//                         name="title" 
+//                         autocomplete="off"/>
+//                     <S.Textarea 
+//                         placeholder="Conteúdo" 
+//                         value={inputValue}
+//                         onChange={handleChange} />
+//                     <S.Button type="submit" text="Publicar"/>
+//                 </div>
+//             </S.Modal>
+//         </div>
+//     )
+// }
